d165e034e547abc6e5b6053f03a1061d860421b8
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-08-19 15:29:39 -0600

    reparacion de bug al cargar dom

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,19 @@
-import {consultaApi} from './api.js';
+import { consultaApi } from './api.js';
 
 const dado = document.querySelector('#dado');
 
-document.addEventListener('DOMContentLoaded', () => {
-    cargarSpinner();
-    setTimeout(() => {
-        muestraDatosAPI();
-    }, 1000);
-});
+setTimeout(() => {
+    muestraDatosAPI();
+}, 1000);
 
-dado.addEventListener('click', () =>{
+
+dado.addEventListener('click', () => {
+    const spinnerExistente = document.querySelector('.lds-ripple');
+    if (spinnerExistente) {
+        spinnerExistente.remove();
+    }
     cargarSpinner();
+    
     setTimeout(() => {
         muestraDatosAPI();
     }, 1000);
@@ -21,8 +24,8 @@ async function muestraDatosAPI() {
     const pTexto = document.querySelector('#texto');
 
     const datos = await consultaApi();
-    const {id,advice} = datos;
-    
+    const { id, advice } = datos;
+
     pAdvice.textContent = `Advice ${id}`;
     pTexto.textContent = `"${advice}"`;
 }
@@ -31,13 +34,14 @@ function cargarSpinner() {
     const pAdvice = document.querySelector('#advice');
     const pTexto = document.querySelector('#texto');
 
-    pAdvice.textContent = 'Loading';
+    pAdvice.textContent = 'Loading...';
     pTexto.textContent = '';
-    
+
     const spinner = document.createElement('div');
     spinner.innerHTML = `
-    <div class="lds-ripple"><div></div><div></div></div>
-    `;
-
+        <div class="lds-ripple"><div></div><div></div></div>
+        `;
     pTexto.appendChild(spinner);
+
+
 }
